091d152cba623220a96b135b3f8c9b49a0c46883
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:32:09 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "stream_analytics_job_storage_accounts_id" {
+  description = "Map of id values across all stream_analytics_job_storage_accounts, keyed the same as var.stream_analytics_job_storage_accounts"
+  value       = { for k, v in azurerm_stream_analytics_job_storage_account.stream_analytics_job_storage_accounts : k => v.id }
+}
 output "stream_analytics_job_storage_accounts_authentication_mode" {
   description = "Map of authentication_mode values across all stream_analytics_job_storage_accounts, keyed the same as var.stream_analytics_job_storage_accounts"
   value       = { for k, v in azurerm_stream_analytics_job_storage_account.stream_analytics_job_storage_accounts : k => v.authentication_mode }
