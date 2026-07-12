a038722937c202e9acdd0008c47358f21dc484c2
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:59:32 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -7,13 +7,17 @@ Required:
     - stream_analytics_job_id
 Optional:
     - storage_account_key
+    - storage_account_key_key_vault_id (alternative to storage_account_key - read from Key Vault instead)
+    - storage_account_key_key_vault_secret_name (alternative to storage_account_key - read from Key Vault instead)
 EOT
 
   type = map(object({
-    authentication_mode     = string
-    storage_account_name    = string
-    stream_analytics_job_id = string
-    storage_account_key     = optional(string)
+    authentication_mode                       = string
+    storage_account_name                      = string
+    stream_analytics_job_id                   = string
+    storage_account_key                       = optional(string)
+    storage_account_key_key_vault_id          = optional(string)
+    storage_account_key_key_vault_secret_name = optional(string)
   }))
 }
 
