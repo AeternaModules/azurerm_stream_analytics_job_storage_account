c303c18c4f52d4635d5fc452932b0e6f0cec4217
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:55 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,18 @@
-output "stream_analytics_job_storage_accounts" {
-  description = "All stream_analytics_job_storage_account resources"
-  value       = azurerm_stream_analytics_job_storage_account.stream_analytics_job_storage_accounts
-  sensitive   = true
-}
 output "stream_analytics_job_storage_accounts_authentication_mode" {
-  description = "List of authentication_mode values across all stream_analytics_job_storage_accounts"
-  value       = [for k, v in azurerm_stream_analytics_job_storage_account.stream_analytics_job_storage_accounts : v.authentication_mode]
+  description = "Map of authentication_mode values across all stream_analytics_job_storage_accounts, keyed the same as var.stream_analytics_job_storage_accounts"
+  value       = { for k, v in azurerm_stream_analytics_job_storage_account.stream_analytics_job_storage_accounts : k => v.authentication_mode }
 }
 output "stream_analytics_job_storage_accounts_storage_account_key" {
-  description = "List of storage_account_key values across all stream_analytics_job_storage_accounts"
-  value       = [for k, v in azurerm_stream_analytics_job_storage_account.stream_analytics_job_storage_accounts : v.storage_account_key]
+  description = "Map of storage_account_key values across all stream_analytics_job_storage_accounts, keyed the same as var.stream_analytics_job_storage_accounts"
+  value       = { for k, v in azurerm_stream_analytics_job_storage_account.stream_analytics_job_storage_accounts : k => v.storage_account_key }
   sensitive   = true
 }
 output "stream_analytics_job_storage_accounts_storage_account_name" {
-  description = "List of storage_account_name values across all stream_analytics_job_storage_accounts"
-  value       = [for k, v in azurerm_stream_analytics_job_storage_account.stream_analytics_job_storage_accounts : v.storage_account_name]
+  description = "Map of storage_account_name values across all stream_analytics_job_storage_accounts, keyed the same as var.stream_analytics_job_storage_accounts"
+  value       = { for k, v in azurerm_stream_analytics_job_storage_account.stream_analytics_job_storage_accounts : k => v.storage_account_name }
 }
 output "stream_analytics_job_storage_accounts_stream_analytics_job_id" {
-  description = "List of stream_analytics_job_id values across all stream_analytics_job_storage_accounts"
-  value       = [for k, v in azurerm_stream_analytics_job_storage_account.stream_analytics_job_storage_accounts : v.stream_analytics_job_id]
+  description = "Map of stream_analytics_job_id values across all stream_analytics_job_storage_accounts, keyed the same as var.stream_analytics_job_storage_accounts"
+  value       = { for k, v in azurerm_stream_analytics_job_storage_account.stream_analytics_job_storage_accounts : k => v.stream_analytics_job_id }
 }
 
